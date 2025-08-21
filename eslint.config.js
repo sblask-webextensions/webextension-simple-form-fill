a69aebd9378ab00033a2dd67c486d99161ecfb1d
Sebastian Blask <blask@gmx.de>
2025-08-22 03:07:01 +1200

    chore: switch stylistic plugin and tweak config for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 const globals = require("globals");
 const js = require("@eslint/js");
-const stylistic = require("@stylistic/eslint-plugin-js");
+const stylistic = require("@stylistic/eslint-plugin");
 
 module.exports = [
     {
@@ -37,6 +37,7 @@ module.exports = [
                 {
                     "args": "all",
                     "argsIgnorePattern": "^_[^_]",
+                    "caughtErrorsIgnorePattern": "^_[^_]",
                     "varsIgnorePattern": "^_[^_]",
                 },
             ],
